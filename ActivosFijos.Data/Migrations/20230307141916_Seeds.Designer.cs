413ba20988ebfbf6723fc27f008f591367f80c2c
Yeisson Mendoza <yeisson001@hotmail.com>
2023-03-07 10:25:43 -0400

    Seeds

--- ActivosFijos.Data/Migrations/20230307141916_Seeds.Designer.cs
+++ ActivosFijos.Data/Migrations/20230307141916_Seeds.Designer.cs
@@ -4,6 +4,7 @@ using ActivosFijos.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ActivosFijos.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230307141916_Seeds")]
+    partial class Seeds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -252,14 +255,6 @@ namespace ActivosFijos.Data.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("User");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 4,
-                            Password = "admin1234",
-                            Username = "admin"
-                        });
                 });
 
             modelBuilder.Entity("ActivosFijos.Model.ActivoFijo", b =>
---
+++ ActivosFijos.Data/Migrations/20230307141916_Seeds.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ActivosFijos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seeds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Departamento",
+                columns: new[] { "Id", "Descripcion", "Estado" },
+                values: new object[,]
+                {
+                    { 1, "Administracion", 0 },
+                    { 2, "Contabilidad", 0 },
+                    { 3, "Informatica", 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Departamento",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Departamento",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Departamento",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
